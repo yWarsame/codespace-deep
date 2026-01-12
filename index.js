d81d0a215f76d9fc576f5125d9efd7c0a10e06b5
Yasin Warsame <yasinhassan1013@gmail.com>
2026-01-12 19:10:06 +0000

    undefined port fixed

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const app = require('./app')
-const port = process.env.PORT || 3000;
-//const {PORT} = process.env
+/**const app = require('./app')
+//const port = process.env.PORT || 3000;
+const {PORT} = process.env
 
 app.listen(PORT, () => {
 
     console.log (`Server is running at port : ${PORT}`);
-})
+})*/
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port : ${PORT}`);
+});
+
